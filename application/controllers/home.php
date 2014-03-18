dbd1ba48f6d868961c79454997aaae2505786c58
jack.distm <jack.distm@gmail.com>
2014-03-19 00:03:31 +0700

    initial commit

---
+++ application/controllers/home.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function index()
+    {
+        echo 'Index';
+    }
+
+}
